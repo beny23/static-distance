953ef86f8eb152f2a0c2c4e89d48984d191c9835
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-07-13 09:41:34 +0100

    try to publish

--- build.sh
+++ build.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+set -ex
 
-rm -f target/*
+rm -rf target/*
+
+tail +2 postcode-outcodes.csv | cut -f2 -d, | while read -r outcode; do
+  d1=${outcode:0:1}
+  d2=${outcode:1:1}
+  mkdir -p target/outcode/$d1/$d2
+  mkdir -p target/pubs/$d1/$d2
+done
+mkdir -p target/outcode/G/I
+mkdir -p target/pubs/G/I
+
 gunzip -c ukpostcodes.csv.gz > target/ukpostcodes.csv
 awk -F, -f split_outcodes.awk target/ukpostcodes.csv
 shuf -n 200000 target/ukpostcodes.csv > target/pubs.csv
+
 paste -d "," <(paste -d " " <(shuf -r animal_names.txt) <(yes "and") <(shuf -r animal_names.txt) | head -200000) target/pubs.csv > target/named_pubs.csv
 
-for f in target/outcode-*.csv
+for f in `find target/outcode -name "*.csv"`
 do
-  outcode_csv=${f:15}
+  outcode_csv=`basename $f`
   awk -F, -f minmax_outcode.awk $f | {
     read -r min_lat min_lon max_lat max_lon
-    echo "name,postcode,lat,lon" > target/pubs_$outcode_csv
-    awk -F, -v OFS=, -v min_lat=$min_lat -v max_lat=$max_lat -v min_lon=$min_lon -v max_lon=$max_lon -f filter_pubs.awk target/named_pubs.csv >> target/pubs_$outcode_csv
+    echo "name,postcode,lat,lon" > target/pubs/$outcode_csv
+    awk -F, -v OFS=, -v min_lat=$min_lat -v max_lat=$max_lat -v min_lon=$min_lon -v max_lon=$max_lon -f filter_pubs.awk target/named_pubs.csv >> target/pubs/$outcode_csv
   }
 done
 
--- split_outcodes.awk
+++ split_outcodes.awk
@@ -1,7 +1,10 @@
-{
+$1 != "id" {
   prev=file;
   split($2, f, " ");
-  file="target/outcode-" f[1] ".csv";
+  outcode = f[1]
+  outcode1 = substr(outcode, 1, 1)
+  outcode2 = substr(outcode, 2, 1)
+  file="target/outcode/" outcode1 "/" outcode2 "/" outcode ".csv";
   if (prev!=file) close(prev);
   if (headers[file] != "done") {
     print "id,postcode,lat,lon" >> file;
