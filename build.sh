5e6a87d55d5ed34a9981ab589074dcfc3735650a
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-07-29 01:14:42 +0100

    fix build to work with GH actions

--- build.sh
+++ build.sh
@@ -17,10 +17,15 @@ gunzip -c ukpostcodes.csv.gz | awk -F, '$3 ~ /^[0-9.-]+$/ && $4 ~ /^[0-9.-]+$/ &
 awk -F, -f split_outcodes.awk target/ukpostcodes.csv
 shuf -n 100000 target/ukpostcodes.csv > target/pubs.csv
 
-paste -d "," <(paste -d " " <(shuf -r animal_names.txt) <(yes "and") <(shuf -r animal_names.txt) | head -100000) target/pubs.csv > target/named_pubs.csv
+shuf -rn 100000 animal_names.txt > target/1.txt
+shuf -rn 100000 animal_names.txt > target/2.txt
+yes "and" | head -100000 > target/and.txt
+paste -d " " target/1.txt target/and.txt target/2.txt > target/pubnames.txt
+
+paste -d "," target/pubnames.txt target/pubs.csv > target/named_pubs.csv
 
 awk -F, -f split_pubs.awk target/named_pubs.csv
 
 cp index.html target
 
-rm target/ukpostcodes.csv target/pubs.csv target/named_pubs.csv
+rm target/ukpostcodes.csv target/pubs.csv target/named_pubs.csv target/*.txt
