7d488eebe98cb0cce2141ff4a5fb365d10b00952
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-07-30 13:48:03 +0100

    Better parsing

--- parse_restaurants.py
+++ parse_restaurants.py
@@ -1,9 +1,9 @@
 import csv
 
 with open('target/restaurants.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, quotechar='"')
+    reader = csv.reader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
     for row in reader:
-        name = row[0].replace("['\",]", "")
+        name = row[0].replace(",", "")
         postcode = row[5].replace(" ", "").upper()
         outcode = postcode[0:-3]
         incode = postcode[-3:]
