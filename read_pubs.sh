f77c8b1dffa186663538242f88ce3130b0f776a2
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-08-01 04:15:21 +0100

    Remove duplicate pubs

--- read_pubs.sh
+++ read_pubs.sh
@@ -2,5 +2,5 @@
 set -ex
 
 curl https://raw.githubusercontent.com/hmrc/eat-out-to-help-out-establishments/master/data/participating-establishments/restaurants.csv > target/restaurants.csv
-python parse_restaurants.py target/restaurants.csv > target/pub_postcodes.csv
+python parse_restaurants.py target/restaurants.csv | sort -u > target/pub_postcodes.csv
 touch target/hmrc_parsed.txt
