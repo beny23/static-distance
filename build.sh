f8a1f30d6efcd8cd06f7e1e112dba6946f8f2b1e
Scott Dutton <exussum12@users.noreply.github.com>
2020-08-04 20:00:40 +0100

    Reduce decimals on locations
    Should shrink the file
    https://xkcd.com/2170/
    4 Decimal places should be plenty

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ do
   mkdir -p target/pubgrid/$d
 done
 
-gunzip -c ukpostcodes.csv.gz | awk -F, -f filter_postcodes.awk > target/ukpostcodes.csv
+gunzip -c ukpostcodes.csv.gz | awk -F, '{printf "%s,%s,%.4f,%.4f\n", $1, $2, $3, $4}' | awk -F, -f filter_postcodes.awk > target/ukpostcodes.csv
 awk -F, -f split_locations.awk target/ukpostcodes.csv
 
 #bash generate_pubs.sh
@@ -19,7 +19,7 @@ join -t , -1 2 -2 2 -o 1.1,0,2.3,2.4 <(sort -k 2 -t , target/pub_postcodes.csv) 
 awk -F, -f filter_postcodes.awk target/named_pubs.csv | awk -F, -f split_pubs.awk
 
 #Add additional bits to "outcodes" for non postcode places
-awk 'BEGIN { FS="\t"; OFS=","; } $8 ~ /^(PPL|ADM)/ && $5 > 49 && $5 < 61 && $6 > -8 && $6 < 2 { gsub(/[^a-zA-Z0-9]/, "", $2); print $8, $2, $5, $6, $15 }' GB.txt \
+awk 'BEGIN { FS="\t"; OFS=","; } $8 ~ /^(PPL|ADM)/ && $5 > 49 && $5 < 61 && $6 > -8 && $6 < 2 { gsub(/[^a-zA-Z0-9]/, "", $2); printf "%s,%s,%.4f,%.4f,%s\n", $8, $2, $5, $6, $15 }' GB.txt \
 | sort -rnk5,5 -t, | cut -f1-4 -d, | tr '[:lower:]' '[:upper:]' | sort -u -k2,2 -t, | awk -F, -f split_locations.awk
 cat postcode-outcodes.csv | awk -F, -f filter_postcodes.awk | awk -F, -f split_locations.awk
 
