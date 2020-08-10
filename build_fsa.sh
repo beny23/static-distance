8eea6db9523dfb6255faff21452f9de6ccef6300
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-08-10 12:49:17 +0100

    Fix FSA lookup

--- build_fsa.sh
+++ build_fsa.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -ex
 
-curl https://github.com/beny23/static-distance-fsa/blob/gh-pages/complete.csv.gz?raw=true | gunzip -dc > target complete.csv
+curl --fail -L https://raw.githubusercontent.com/beny23/static-distance-fsa/gh-pages/complete.csv.gz | gunzip -dc > target complete.csv
 
 python3 parse_restaurants.py > target/pub_postcodes.csv
 
