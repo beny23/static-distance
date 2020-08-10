cca8b02ba8e94cf78355284f1b93d21a7c976ab2
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-08-10 10:17:06 +0100

    FSA rating refactor (#22)
    * Add FSA Ratings
    
    This also prefers FSA long lat so its based on locations not postcodes
    
    * Rename FSA
    
    * Use Image instead
    
    * Add List page
    
    * Refactor of FSA integration
    
    * Refactor of FSA integration
    
    Co-authored-by: Scott Dutton <scott@exussum.co.uk>
    Co-authored-by: Scott Dutton <exussum12@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@ set -ex
 
 rm -rf target/*
 
-for d in `seq 0 170`
-do
-  mkdir -p target/pubgrid/$d
-done
+mkdir -p target/pubgrid/{0..170}
 
 gunzip -c ukpostcodes.csv.gz | awk -F, -f reduce_precision.awk | awk -F, -f filter_postcodes.awk > target/ukpostcodes.csv
 awk -F, -f split_locations.awk target/ukpostcodes.csv
@@ -14,9 +11,11 @@ awk -F, -f split_locations.awk target/ukpostcodes.csv
 #bash generate_pubs.sh
 bash read_pubs.sh
 
-join -t , -1 2 -2 2 -o 1.1,0,2.3,2.4 <(sort -k 2 -t , target/pub_postcodes.csv) <(sort -k 2 -t , target/ukpostcodes.csv) > target/named_pubs.csv
+bash build_fsa.sh
 
-awk -F, -f filter_postcodes.awk target/named_pubs.csv | awk -F, -f split_pubs.awk
+awk -F, -f filter_chains.awk target/complete.csv >> target/named_pubs.csv
+
+awk -F, -f filter_postcodes.awk target/named_pubs.csv | cut -f1-4 -d, | sort -uf | sort -t, -k2,2 | awk -F, -f split_pubs.awk
 
 #Add additional bits to "outcodes" for non postcode places
 awk 'BEGIN { FS="\t"; OFS=","; } $8 ~ /^(PPL|ADM)/ && $5 > 49 && $5 < 61 && $6 > -8 && $6 < 2 { gsub(/[^a-zA-Z0-9]/, "", $2); print $8, $2, $5, $6, $15 }' GB.txt \
@@ -28,5 +27,5 @@ rm target/*.csv target/*.txt
 bash build_sitemap.sh
 
 cp index.html target/
-cp assets/* target/
+cp -R assets/* target/
 
--- parse_restaurants.py
+++ parse_restaurants.py
@@ -1,10 +1,11 @@
 import csv
 
-with open('target/restaurants.csv', 'rb') as csvfile:
+with open('target/restaurants.csv', 'rt') as csvfile:
     reader = csv.reader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
     for row in reader:
-        name = row[0].replace(",", "")
+        name = row[0].replace(",", " ")
         postcode = row[5].replace(" ", "").upper()
+        address1 = row[1].replace(",", " ")
         outcode = postcode[0:-3]
         incode = postcode[-3:]
-        print(name + "," + outcode + " " + incode)
+        print(name + "," + address1 + "," + outcode + " " + incode)
